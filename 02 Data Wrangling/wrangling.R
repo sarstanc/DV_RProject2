73958759e6f7d46c4403380973b6263960be2e6a
Sarah Stancik <skstancik@utexas.edu>
2015-02-12 17:06:19 -0600

    r file for data sample

--- 02 Data Wrangling/wrangling.R
+++ 02 Data Wrangling/wrangling.R
@@ -8,8 +8,6 @@ require(dplyr)
 tbl_df(df)
 df <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from apd_incidents"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_sks2435', PASS='orcl_sks2435', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 
-#check data frames individually
-
 # MOST POPULAR CRIMES BY PERCENTAGE
 dfPERCENT<- df %>% group_by(CRIME_TYPE) %>% summarise(Percentage = (n()/2833)*100, n = n()) %>% arrange(desc(n)) %>% tbl_df()
 dfPERCENT
